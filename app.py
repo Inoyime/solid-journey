c908d7871a0506a55d4e84268ded5a67b0b3ef33
Inoyime <125993107+Inoyime@users.noreply.github.com>
2023-02-21 17:50:32 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Título")
+opciones=("o1","o2","o3")
+respuesta1=st.radio("Pregunta",opciones)
+
+if respuesta1=="o2":
+  st.write("estupendo")
+if respuesta1=="o1":
+  st.write("no, eso no es una opción")
+if respuesta=="o3":
+  st.write("ok")
