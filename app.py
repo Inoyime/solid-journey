5f3b49e12c0831dbaaabe31b2abac6a041d2dad6
Inoyime <125993107+Inoyime@users.noreply.github.com>
2023-02-21 18:05:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 st.title("¿Tienes poca imaginación para un título?")
-opciones=("No","Sí","Meh")
+opciones=("¿No?","Sí","Meh")
 respuesta1=st.radio("Pregunta",opciones)
 
 if respuesta1=="No":
-  st.write("Estupendo")
+  st.write("Dame tu imaginación")
 if respuesta1=="Sí":
-  st.write("Ok")
+  st.write("Ok...Ya somos dos...")
   st.balloons()
 if respuesta1=="Meh":
-  st.write("Vale")
+  st.write("Vale, ni pa' ti ni pa' mí")
